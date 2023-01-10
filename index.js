234bcc9c1c92af4def93637aee3d01e2f6b992f3
Shane Siebler <ssiebler21@gmail.com>
2023-01-10 09:13:04 -0700

    client does not support authentication protocol, stuck

--- index.js
+++ index.js
@@ -3,10 +3,32 @@ const app = express();
 
 const mysql = require("mysql");
 
-const db = require("./models");
-
-db.sequelize.sync().then((req) => {
-    app.listen(3001, () => {
-        console.log("server running");
-    });
+// const db = require("./models");
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    database: "FakeDatabase",
 });
+
+app.get('/insert', (req, res) => {
+
+    db.query('INSERT INTO countries (countryName, population) VALUES ("USA", 300000000)', (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+
+        res.send(result);
+        }
+    );
+});
+
+app.listen(3001, () => {
+    console.log("server running");
+});
+
+// db.sequelize.sync().then((req) => {
+//     app.listen(3001, () => {
+//         console.log("server running");
+//     });
+// });
